2c7cc968cfe5c2d4d483b39469332bace5b4de2c
ecomloop <andrew@ecomloop.com>
2020-04-27 13:15:35 -0700

    add random shop template

---
+++ src/pages/randomshop.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
+import { navigate } from "@reach/router"
+
+const RandomShop = ({ data }) => {
+  const { edges } = data.allGoogleSheetListRow;
+
+  console.log("Total Shops = "+edges.length);
+  const randomnumber = Math.round(Math.random() * edges.length);
+  console.log("Generated Random Number = "+randomnumber);
+  const edge = edges[randomnumber-1] ? edges[randomnumber-1] : edges[0];
+  const randomshopurl = "/shops/"+edge.node.name;
+  console.log("Random URL = "+randomshopurl);
+  navigate(randomshopurl);
+
+  return (
+    <Helmet title={'Random Shop'} />
+  );
+
+};
+
+export default RandomShop;
+
+export const query = graphql`
+  query {
+    allGoogleSheetListRow {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`;
