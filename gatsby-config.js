15b77d3b1fa9f5cf9a71caaf401d7632e2cb7a2e
andrew@ecomloop <andrew@ecomloop.com>
2020-09-25 13:04:51 -0700

    update pkgs

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,11 +49,11 @@ module.exports = {
       resolve: `gatsby-source-mysql`,
       options: {
         connectionDetails: {
-          host: 'db-emp22-do-user-7599813-0.a.db.ondigitalocean.com',
-          port: '25060',
-          user:  'emp22-dbuser',
-          password:  'qkjxw1eigibsha4w',
-          database:  'emp22-db-primary'
+          host: process.env.MYSQL_HOST,
+          port: process.env.MYSQL_PORT,
+          user:  process.env.MYSQL_USER,
+          password: process.env.MYSQL_PASSWD,
+          database: process.env.MYSQL_DB
         },
         queries: [
           {
