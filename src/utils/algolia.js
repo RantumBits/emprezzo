50a27a9d39751446ad5f871db4a8072958c1c6a2
andrew @ ecomloop <andrew@ecomloop.com>
2020-11-16 07:58:39 -0800

    increase records to 7000

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -21,7 +21,7 @@ const shopQuery = `{
 }`
 
 const productQuery = `{
-  products: allMysqlShopifyProductsAll(limit: 1000) {
+  products: allMysqlShopifyProductsAll(limit: 7000) {
     edges {
       node {
         id: UniqueKey
