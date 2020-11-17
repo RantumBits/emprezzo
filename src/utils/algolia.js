519d61fe2e8c399db88cc3826d9fea256e1be1b8
andrew @ ecomloop <andrew@ecomloop.com>
2020-11-17 11:24:33 -0800

    revise to 6500

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -21,7 +21,7 @@ const shopQuery = `{
 }`
 
 const productQuery = `{
-  products: allMysqlShopifyProductsAll(limit: 5500) {
+  products: allMysqlShopifyProductsAll(limit: 6500) {
     edges {
       node {
         id: UniqueKey
